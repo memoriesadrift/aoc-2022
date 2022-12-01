f1e1cd8c2d64d3f2f5b1e1362a8160f71eedd0de
Samuel Šulovský <samuel.sulovsky@gmail.com>
2022-12-01 10:29:53 +0100

    Solve day 1 🎄

--- src/day1.ts
+++ src/day1.ts
@@ -1,3 +1,18 @@
-import {getInput} from './util/io.ts'
+import {getInputSplitBy, printSolutions} from './util/io.ts'
 
-const input = await getInput(1, false)
+const input = (await getInputSplitBy(1, '\n\n', false))
+                  .filter((s) => s !== '')
+                  .map((elfCarry) => elfCarry.split('\n').map((a) => Number.parseInt(a)))
+                  .map((carryArray) => carryArray.reduce((acc, curr) => acc + curr, 0))
+
+const solve1 = (input: number[]) => {
+  return input.reduce((max, curr) => max > curr ? max : curr, 0)
+}
+
+const solve2 = (input: number[]) => {
+  return input.sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((acc, curr) => acc + curr, 0)
+}
+
+printSolutions(solve1, solve2, input)
--- src/util/io.ts
+++ src/util/io.ts
@@ -1,8 +1,31 @@
+/*
+ * 
+ * Input
+ *
+ */
 const getInputFromFile = async (day: number, test = false) => await Deno.readTextFile(`res/day${day}.in${test ? '.test' : ''}`)
 
 export const getInput = async (day: number, test = false) => (await getInputFromFile(day, test)).split('\n')
+
+export const getInputSplitBy = async (day: number, split: string, test = false) => (await getInputFromFile(day, test)).split(split)
 
 export const getInputAsIntList = async (day: number, test = false) => (await getInput(day, test)).map((e) => Number.parseInt(e))
 
 export const getInputAsCommaSeparatedIntList = async (day: number, test = false) => (await getInputFromFile(day, test)).split(',').map((e) => Number.parseInt(e))
 
+
+/*
+ * 
+ * Output
+ *
+ */
+
+// deno-lint-ignore ban-types no-explicit-any
+export const printSolutions = (solve1: Function, solve2: Function, input: any) => {
+  console.log(
+    "Task 1: ", solve1(input)
+  )
+  console.log(
+    "Task 2: ", solve2(input)
+  )
+}
